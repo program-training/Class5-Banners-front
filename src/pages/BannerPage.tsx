7afcd9fb0e568bd439ebb8f10273eee6ad5a6204
Smuel18 <shh92533@gmail.com>
2023-11-21 16:54:26 +0200

    add banner

--- src/pages/BannerPage.tsx
+++ src/pages/BannerPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { styled } from "@mui/material/styles";
+import { styled } from "@mui/system";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
@@ -12,47 +12,53 @@ const Img = styled("img")({
   maxHeight: "100%",
 });
 
-const BannerPage = () => {
+interface ResponsiveAdProps {
+  imageUrl: string;
+  title: string;
+  description: string;
+  link: string;
+  note?: string;
+}
+
+const BannerPage: React.FC<ResponsiveAdProps> = ({
+  imageUrl,
+  title,
+  description,
+  note,
+}) => {
   return (
     <Paper
       sx={{
         p: 2,
         margin: "auto",
-        maxWidth: 3000,
-        maxHeight: 3000,
+        // height: "100vh",
+        // width: "100Vw",
+        flexGrow: 2,
         backgroundColor: (theme) =>
           theme.palette.mode === "dark" ? "#1A2027" : "#fff",
       }}
     >
-      <Grid container spacing={2} justifyContent="center">
+      <Grid container spacing={2}>
         <Grid item>
-          <ButtonBase sx={{ width: 128, height: 128 }}>
-            <Img alt="complex" src="https://placekitten.com/800/400" />
+          <ButtonBase sx={{ width: "100%", height: "auto" }}>
+            <Img alt="ad" src={imageUrl} />
           </ButtonBase>
         </Grid>
         <Grid item xs={12} sm container>
           <Grid item xs container direction="column" spacing={2}>
             <Grid item xs>
-              <Typography gutterBottom variant="h4" component="div">
-                {" "}
-                כותרת המודעה
+              <Typography gutterBottom variant="h6" component="div">
+                {title}
               </Typography>
               <Typography variant="body1" gutterBottom>
-                זהו המקום לתיאור המודעה שלך. כתוב כמה משפטים שיתארו בקצרה את
-                התוכן והיתרונות של המוצר או השירות שאתה מציע.
+                {description}
               </Typography>
+              {note && (
+                <Typography variant="body2" color="text.secondary">
+                  {note}
+                </Typography>
+              )}
             </Grid>
-            <Grid item>
-              <Typography sx={{ cursor: "pointer" }} variant="body2">
-                מידע נוסף
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Typography variant="h5" component="div">
-              {" "}
-              $19.00
-            </Typography>
           </Grid>
         </Grid>
       </Grid>
