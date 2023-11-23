042a8443f42d3fe26ab9010d0556bc2049d22bea
moshedoytsh <moshedoytsh@gmail.com>
2023-11-23 16:11:36 +0200

    import react removed

--- src/features/users/pages/ShowUserPage.tsx
+++ src/features/users/pages/ShowUserPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
