012ca815d70d8b1f5c48e9316211ba28a5f0debd
Smuel18 <shh92533@gmail.com>
2023-11-23 16:03:56 +0200

    show and edit user

--- src/features/users/pages/ShowUserPage.tsx
+++ src/features/users/pages/ShowUserPage.tsx
@@ -1,15 +1,52 @@
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import { useAppSelector } from "../../../redux/hooks";
 
 const ShowUserPage = () => {
+  const navigate = useNavigate();
+  const user = useAppSelector((state) => state.user);
+
+  const [data, setData] = useState({ username: "", email: "", isAdmin: false });
+  if (!user.loggedIn) {
+    navigate("/my-banners/bla-bla");
+  }
+  useEffect(() => {
+    try {
+      axios.get("YOUR_API_ENDPOINT").then((response) => setData(response.data));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }, []);
+
   return (
-    <Box>
-      <Typography variant="h2"> Banner Shop </Typography>
-      <Typography variant="h5"> Show User </Typography>
-      {/* <Link to={'/SignUp'} component={<SignUpPage/>} >
-          {Children}
-        </Link> */}
-    </Box>
+    <Paper
+      elevation={3}
+      sx={{
+        padding: "20px",
+        maxWidth: "400px",
+        margin: "auto",
+        marginTop: "50px",
+        backgroundColor: "#f5f5f8",
+        borderRadius: "10px",
+        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <Typography variant="h4" gutterBottom sx={{ color: "#333" }}>
+        User Profile
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom sx={{ color: "#555" }}>
+        Username: {data.username}
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom sx={{ color: "#555" }}>
+        Email: {data.email}
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom sx={{ color: "#555" }}>
+        Admin Status: {data.isAdmin ? "Yes" : "No"}
+      </Typography>
+    </Paper>
   );
 };
 
