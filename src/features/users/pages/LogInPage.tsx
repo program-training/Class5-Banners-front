92f43f8b3b9f6cd70ac2e0024c20046d630da610
YaakovMoyal <kobi856m@gmail.com>
2023-11-26 13:52:36 +0200

    my banner

--- src/features/users/pages/LogInPage.tsx
+++ src/features/users/pages/LogInPage.tsx
@@ -8,62 +8,61 @@ import { Grid, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const LogIn = () => {
-    const [email, setEmail] = useState("");
-    const [isValidEmail, setIsValidEmail] = useState(true);
-    const [password, setPassword] = useState("");
-    const [isValidPassword, setIsValidPassword] = useState(true);
+  const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [password, setPassword] = useState("");
+  const [isValidPassword, setIsValidPassword] = useState(true);
 
-    const isAllValid = email && isValidEmail && password && isValidPassword;
-    return (
-        <Grid
-            container
-            spacing={2}
-            justifyContent="center"
-            sx={{ padding: "20px", mt: 2 }}
+  const isAllValid = email && isValidEmail && password && isValidPassword;
+  return (
+    <Grid
+      container
+      spacing={2}
+      justifyContent="center"
+      sx={{ padding: "20px", mt: 2 }}
+    >
+      <Grid
+        xs={10}
+        md={6}
+        lg={4}
+        style={{
+          padding: "20px",
+          backgroundColor: "#f9f9f9",
+          borderRadius: "8px",
+        }}
+      >
+        <LoginTop />
+        <EmailInput
+          email={email}
+          setEmail={setEmail}
+          isValidEmail={isValidEmail}
+          setIsValidEmail={setIsValidEmail}
+        />
+        <PasswordInputs
+          password={password}
+          setPassword={setPassword}
+          isValidPassword={isValidPassword}
+          setIsValidPassword={setIsValidPassword}
+        />
+        {isAllValid ? <SignUpSubmitButton /> : <FormError />}
+        <Typography
+          variant="body2"
+          align="center"
+          style={{ marginTop: "10px" }}
         >
-            <Grid
-                xs={10}
-                md={6}
-                lg={4}
-                style={{
-                    padding: "20px",
-                    backgroundColor: "#f9f9f9",
-                    borderRadius: "8px",
-                }}
-            >
-                <LoginTop />
-                <EmailInput
-                    email={email}
-                    setEmail={setEmail}
-                    isValidEmail={isValidEmail}
-                    setIsValidEmail={setIsValidEmail}
-                />
-                <PasswordInputs
-                    password={password}
-                    setPassword={setPassword}
-                    isValidPassword={isValidPassword}
-                    setIsValidPassword={setIsValidPassword}
-                />
-                {isAllValid ? <SignUpSubmitButton /> : <FormError />}
-                <Typography
-                    variant="body2"
-                    align="center"
-                    style={{ marginTop: "10px" }}
-                >
-                    Don't have am account?
-                    <Link to="/user/sign-up"> Sign Up</Link>
-                </Typography>
-                <Typography
-                    variant="body2"
-                    align="center"
-                    style={{ marginTop: "10px" }}
-                >
-                    Forgot password?
-                    <Link to="/user/forget-password"> Create new password</Link>
-                </Typography>
-            </Grid>
-        </Grid>
-    );
+          Don't have am account?
+          <Link to="/user/sign-up"> Sign Up</Link>
+        </Typography>
+        <Typography
+          variant="body2"
+          align="center"
+          style={{ marginTop: "10px" }}
+        >
+          <Link to="/user/forget-password">Forgot password?</Link>
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LogIn;
