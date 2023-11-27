cc22ae601b7dbce56b63485ac02af2ff75032a04
Yehuda Koth <ykot@gmail.com>
2023-11-27 10:12:50 +0200

    update management table

--- src/features/banners/pages/BannerManagementPage.tsx
+++ src/features/banners/pages/BannerManagementPage.tsx
@@ -7,30 +7,30 @@ import DeleteBannerDialog from "../components/DeleteBannerDialog";
 import { getBannersFromServer } from "../service/getBanners";
 
 const BannerManagementPage = () => {
-  const [openDialog, setOpenDialog] = useState(false);
-  const [banners, setBanners] = useState(data);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [banners, setBanners] = useState(data);
 
-  useEffect(() => {
-    getBannersFromServer()
-      .then((res) => {
-        setBanners(res);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+    useEffect(() => {
+        getBannersFromServer()
+            .then((res) => {
+                setBanners(res);
+            })
+            .catch((err) => console.log(err));
+    }, []);
 
-  return (
-    <Container maxWidth="md">
-      <Typography variant="h2" padding={2} align="center">
-        Banner Management
-      </Typography>
-      <BannerManagementTop setBanners={setBanners} />
-      <BannerTable data={banners} setOpenDialog={setOpenDialog} />
-      <DeleteBannerDialog
-        openDialog={openDialog}
-        setOpenDialog={setOpenDialog}
-      />
-    </Container>
-  );
+    return (
+        <Container maxWidth="md">
+            <Typography variant="h2" padding={2} align="center">
+                Banner Management
+            </Typography>
+            <BannerManagementTop setBanners={setBanners} />
+            <BannerTable data={banners} setOpenDialog={setOpenDialog} />
+            <DeleteBannerDialog
+                openDialog={openDialog}
+                setOpenDialog={setOpenDialog}
+            />
+        </Container>
+    );
 };
 
 export default BannerManagementPage;
