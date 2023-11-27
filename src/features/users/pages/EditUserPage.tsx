6092a58b88a6f0f77819dddd784cdae3e1d0ea94
Smuel18 <shh92533@gmail.com>
2023-11-27 15:12:13 +0200

    bugfix

--- src/features/users/pages/EditUserPage.tsx
+++ src/features/users/pages/EditUserPage.tsx
@@ -12,57 +12,57 @@ import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
 import { UserInterface } from "../interfaces/userInterface";
 
 const EditUserPage = () => {
-    const navigate = useNavigate();
-    const user = useAppSelector((state) => state.user);
+  const navigate = useNavigate();
+  const user = useAppSelector((state) => state.user);
 
-    const { register, handleSubmit, setValue } = useForm();
+  const { register, handleSubmit, setValue } = useForm();
 
-    const [userData, setUserData] = useState<UserInterface>({
-        username: "yehuda400",
-        _id: "322418203",
-        email: "ykoth04@gmail.com",
-        isAdmin: true,
-    });
+  const [userData, setUserData] = useState<UserInterface>({
+    username: "yehuda400",
+    _id: "322418203",
+    email: "ykoth04@gmail.com",
+    isAdmin: true,
+  });
 
-    useEffect(() => {
-        if (!user.loggedIn) {
-            navigate("/user/login");
-        } else {
-            axios
-                .get(`/api/users`, {
-                    headers: { Authorization: `${user.token}` },
-                })
-                .then((response) => {
-                    setUserData(response.data);
-                    setValue("Username", response.data.username); // Use "Username" here
-                    setValue("isAdmin", response.data.isAdmin === true);
-                });
-        }
-    }, []);
+  useEffect(() => {
+    if (!user.loggedIn) {
+      navigate("/user/login");
+    } else {
+      axios
+        .get(`/api/users`, {
+          headers: { Authorization: `${user.token}` },
+        })
+        .then((response) => {
+          setUserData(response.data);
+          setValue("Username", response.data.username); // Use "Username" here
+          setValue("isAdmin", response.data.isAdmin === true);
+        });
+    }
+  }, []);
 
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        const updatedUserData = {
-            ...userData,
-            username: data.Username, // Assuming the form field is named 'Username'
-            isAdmin: data.isAdmin ? true : false, // Assuming 'isAdmin' is a boolean
-        };
-
-        console.log(updatedUserData);
-
-        // axios
-        //     .put(`/api/users`, {
-        //         data: {
-        //             updatedUserData,
-        //         },
-        //         headers: { Authorization: `${user.token}` },
-        //     })
-        //     .then((response) => {
-        //         console.log("User data updated successfully:", response.data);
-        //     })
-        //     .catch((error) => {
-        //         console.error("Error updating user data:", error);
-        //     });
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    const updatedUserData = {
+      ...userData,
+      username: data.Username, // Assuming the form field is named 'Username'
+      isAdmin: data.isAdmin ? true : false, // Assuming 'isAdmin' is a boolean
     };
+
+    console.log(updatedUserData);
+
+    // axios
+    //     .put(`/api/users`, {
+    //         data: {
+    //             updatedUserData,
+    //         },
+    //         headers: { Authorization: `${user.token}` },
+    //     })
+    //     .then((response) => {
+    //         console.log("User data updated successfully:", response.data);
+    //     })
+    //     .catch((error) => {
+    //         console.error("Error updating user data:", error);
+    //     });
+  };
 
   return (
     <Container
@@ -89,14 +89,14 @@ const EditUserPage = () => {
         variant="outlined"
         fullWidth
         {...register("name")}
-        defaultValue={userData.name}
+        defaultValue={userData.username}
         sx={{ mb: 2 }}
       />
       <FormControlLabel
         control={
           <Checkbox
             {...register("isAdmin")}
-            defaultChecked={userData.isAdmin === "true"}
+            defaultChecked={userData.isAdmin === true}
           />
         }
         label={"Admin"}
