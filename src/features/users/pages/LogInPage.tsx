a441b47fd59b33bacbad7c05965eadfb6ab9c6de
Smuel18 <shh92533@gmail.com>
2023-11-27 14:49:10 +0200

    Corrected login page

--- src/features/users/pages/LogInPage.tsx
+++ src/features/users/pages/LogInPage.tsx
@@ -67,7 +67,6 @@ const LogIn = () => {
           <SignUpSubmitButton onClick={handleLogin} />
         ) : (
           <>
-            <SignUpSubmitButton onClick={() => {}} />
             <FormError />
           </>
         )}
