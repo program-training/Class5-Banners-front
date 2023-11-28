8d5944318ce20023e6414274abb48c4ee0e78185
Yehuda Koth <ykot@gmail.com>
2023-11-28 14:00:23 +0200

    display user connect to server

--- src/features/users/pages/ShowUserPage.tsx
+++ src/features/users/pages/ShowUserPage.tsx
@@ -1,55 +1,109 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
 import axios from "axios";
 import { useAppSelector } from "../../../redux/hooks";
+import { Box, Container, SxProps, Typography, IconButton } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import Pending from "../../banners/components/Pending";
+import EditIcon from "@mui/icons-material/Edit";
+import HomeIcon from "@mui/icons-material/Home";
+
+const containerStyle: SxProps = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "20px",
+    textAlign: "center",
+    padding: "20px",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+    margin: "20px",
+};
 
 const ShowUserPage = () => {
-  const navigate = useNavigate();
-  const user = useAppSelector((state) => state.user);
-  const [data, setData] = useState({ username: "", email: "", isAdmin: false });
+    const navigate = useNavigate();
+    const user = useAppSelector((state) => state.user);
+    const [data, setData] = useState({
+        username: "test",
+        isAdmin: true,
+        email: "test",
+    });
+    const [status, setStatus] = useState<
+        "none" | "pending" | "success" | "error"
+    >("none");
 
-  useEffect(() => {
-    if (!user.loggedIn) {
-      navigate("/user/login");
-    } else
-      try {
-        axios
-          .get("YOUR_API_ENDPOINT")
-          .then((response) => setData(response.data));
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-  }, [user.loggedIn, navigate]);
+    useEffect(() => {
+        if (!user.loggedIn) navigate("/user/login");
+        setStatus("pending");
+        try {
+            axios
+                .get(
+                    `${import.meta.env.VITE_SERVER_HOST}:${
+                        import.meta.env.VITE_SERVER_PORT
+                    }/api/users`,
+                    {
+                        headers: { Authorization: `${user.token}` },
+                    }
+                )
+                .then((response) => {
+                    setData(response.data);
+                    setStatus("success");
+                })
+                .catch(() => {
+                    setStatus("error");
+                });
+        } catch (error) {
+            setStatus("error");
+        }
+    }, [user.loggedIn, navigate]);
 
-  return (
-    <Paper
-      elevation={3}
-      sx={{
-        padding: "20px",
-        maxWidth: "400px",
-        margin: "auto",
-        marginTop: "50px",
-        backgroundColor: "#f5f5f8",
-        borderRadius: "10px",
-        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      <Typography variant="h4" gutterBottom sx={{ color: "#333" }}>
-        User Profile
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom sx={{ color: "#555" }}>
-        Username: {data.username}
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom sx={{ color: "#555" }}>
-        Email: {data.email}
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom sx={{ color: "#555" }}>
-        Admin Status: {data.isAdmin ? "Yes" : "No"}
-      </Typography>
-    </Paper>
-  );
+    const handleEdit = () => {
+        navigate("/user/edit");
+    };
+
+    const handleHome = () => {
+        navigate("");
+    };
+
+    return (
+        <>
+            <Container maxWidth="sm">
+                <Box sx={containerStyle}>
+                    <Box display={"flex"} justifyContent={"space-between"}>
+                        <IconButton onClick={handleEdit}>
+                            <EditIcon />
+                        </IconButton>
+                        <IconButton onClick={handleHome}>
+                            <HomeIcon />
+                        </IconButton>
+                    </Box>
+
+                    <Typography variant="h4" sx={{ mb: "8px" }}>
+                        Hi, {data.username}!
+                    </Typography>
+                    <Typography variant="subtitle1" sx={{ mb: "20px" }}>
+                        See your profile here
+                    </Typography>
+                    {status === "error" && (
+                        <Alert severity="error" sx={{ m: "20px" }}>
+                            Can't retrieve user data from the server. Please try
+                            again later.
+                        </Alert>
+                    )}
+                    {status === "pending" && <Pending />}
+                    {status === "success" && (
+                        <>
+                            <Typography variant="subtitle1">
+                                Email: {data.email}
+                            </Typography>
+                            <Typography variant="subtitle1">
+                                Admin Status: {data.isAdmin ? "Yes" : "No"}
+                            </Typography>
+                        </>
+                    )}
+                    <Box sx={{ mt: "auto", textAlign: "center" }}></Box>
+                </Box>
+            </Container>
+        </>
+    );
 };
 
 export default ShowUserPage;
