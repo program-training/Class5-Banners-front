46e35b96453ff4b0631160c975c94ef92b5bc39f
itzik <itsikzigi@gmail.com>
2023-11-30 11:13:13 +0200

    some bug fixed

--- src/features/users/service/localStorageService.ts
+++ src/features/users/service/localStorageService.ts
@@ -1,10 +1,15 @@
 import { jwtDecode } from "jwt-decode";
 import { UserInterface } from "../interfaces/userInterface";
 
+export const setItem = (key: string, value: string) =>
+  localStorage.setItem(key, value);
+
 export const getToken = () => localStorage.getItem("token") || null;
 
 export const getUser = (): UserInterface | null => {
   const token = getToken();
-  return jwtDecode(token as string) || null;
+  if (!token) return null;
+  return jwtDecode(token);
 };
-export const deleteToken = () => localStorage.removeItem("token");
+
+export const removeToken = () => localStorage.removeItem("token");
