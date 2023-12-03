b372966b544646d70bbaa659f426b5bde8a6affd
Yehuda Koth <ykot@gmail.com>
2023-12-03 09:27:40 +0200

    inputPassword visibility test

--- src/features/users/components/PasswordInput.tsx
+++ src/features/users/components/PasswordInput.tsx
@@ -4,52 +4,52 @@ import { Dispatch, SetStateAction } from "react";
 import { useState } from "react";
 import { validatePassword } from "../../utils/functions";
 type Props = {
-  setIsValidPassword: Dispatch<SetStateAction<boolean>>;
-  isValidPassword: boolean;
-  setPassword: Dispatch<SetStateAction<string>>;
-  password: string;
+    setIsValidPassword: Dispatch<SetStateAction<boolean>>;
+    isValidPassword: boolean;
+    setPassword: Dispatch<SetStateAction<string>>;
+    password: string;
 };
 const PasswordInputs = ({
-  setIsValidPassword,
-  isValidPassword,
-  setPassword,
-  password,
+    setIsValidPassword,
+    isValidPassword,
+    setPassword,
+    password,
 }: Props) => {
-  const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
-  function handlePasswordChange(value: string): void {
-    setPassword(value);
-    setIsValidPassword(validatePassword(value));
-  }
+    function handlePasswordChange(value: string): void {
+        setPassword(value);
+        setIsValidPassword(validatePassword(value));
+    }
 
-  return (
-    <TextField
-      label="Password"
-      variant="outlined"
-      type={showPassword ? "text" : "password"}
-      fullWidth
-      value={password}
-      onChange={(e) => handlePasswordChange(e.target.value)}
-      error={!isValidPassword}
-      helperText={
-        !isValidPassword &&
-        "Password requirements: 1 uppercase, 1 lowercase, 4 numbers, 1 symbol"
-      }
-      style={{ marginBottom: "10px" }}
-      InputProps={{
-        endAdornment: (
-          <InputAdornment position="end">
-            <IconButton
-              onClick={() => setShowPassword(!showPassword)}
-              onMouseDown={(e) => e.preventDefault()}
-            >
-              {showPassword ? <VisibilityOff /> : <Visibility />}
-            </IconButton>
-          </InputAdornment>
-        ),
-      }}
-    />
-  );
+    return (
+        <TextField
+            label="Password"
+            variant="outlined"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            value={password}
+            onChange={(e) => handlePasswordChange(e.target.value)}
+            error={!isValidPassword}
+            helperText={
+                !isValidPassword &&
+                "Password requirements: 1 uppercase, 1 lowercase, 4 numbers, 1 symbol"
+            }
+            style={{ marginBottom: "10px" }}
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton
+                            onClick={() => setShowPassword(!showPassword)}
+                            // onMouseDown={(e) => e.preventDefault()}
+                        >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                ),
+            }}
+        />
+    );
 };
 
 export default PasswordInputs;
