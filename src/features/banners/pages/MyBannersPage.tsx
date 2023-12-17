5e9e76252adb8776ce920830e1f49eef5003b46c
itzik <itsikzigi@gmail.com>
2023-12-17 15:51:59 +0200

    path updated to graphql

--- src/features/banners/pages/MyBannersPage.tsx
+++ src/features/banners/pages/MyBannersPage.tsx
@@ -22,10 +22,10 @@ const MyBannersPage = () => {
 
   if (!user) navigate(ROUTES.LogInPage);
   useEffect(() => {
-    //   if (!user) return navigate(ROUTES.LogInPage);
     dispatch(getMyBannersReq());
   }, []);
 
+  if (pending) return <Pending />;
   return (
     <Container maxWidth="md">
       <Typography variant="h2" padding={2} align="center">
@@ -39,7 +39,6 @@ const MyBannersPage = () => {
       {bannersState && (
         <BannerTable setOpenDialog={setBannerToDelete} page="my-banners" />
       )}
-      {pending && <Pending />}
       {!pending && !error && !bannersState && (
         <Alert severity="info">You hadn't created banners yet.</Alert>
       )}
