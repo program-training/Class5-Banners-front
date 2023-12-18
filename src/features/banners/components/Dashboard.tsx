d83e51fb6280c5f7d2fb136701b29238c9670d89
Yehuda Koth <ykot@gmail.com>
2023-12-18 14:52:56 +0200

    horizontal

--- src/features/banners/components/Dashboard.tsx
+++ src/features/banners/components/Dashboard.tsx
@@ -3,10 +3,14 @@ import { BarChart } from "@mui/x-charts";
 
 export default function Dashboard() {
     const series = [{ data: [50, 256, 98, 522, 425, 396, 85, 456, 52] }];
+    let height = 0;
+    series[0].data.forEach(() => {
+        height += 50;
+    });
 
     return (
         <Box sx={{ width: "100%" }}>
-            <BarChart height={300} series={series} />
+            <BarChart height={height} series={series} layout="horizontal" />
         </Box>
     );
 }
