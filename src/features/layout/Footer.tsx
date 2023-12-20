7334f24b0728249148e15fb3a50f404114456929
Yehuda Koth <ykot@gmail.com>
2023-12-20 10:12:51 +0200

    github

--- src/features/layout/Footer.tsx
+++ src/features/layout/Footer.tsx
@@ -2,40 +2,50 @@ import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import ROUTES from "../router/routes";
 import {
-  Email,
-  Extension,
-  Facebook,
-  Instagram,
-  Phone,
-  Twitter,
+    Email,
+    Extension,
+    Facebook,
+    Instagram,
+    GitHub,
+    Phone,
+    Twitter,
 } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
-  const navigate = useNavigate();
-  return (
-    <BottomNavigation
-      sx={{
-        width: "100%",
-        backgroundColor: "lightsteelblue",
-        zIndex: 2,
-      }}
-      showLabels
-    >
-      <BottomNavigationAction
-        onClick={() => navigate(ROUTES.home)}
-        label="management banner"
-        icon={<Extension />}
-        title="title for test"
-      />
-      <BottomNavigationAction label="050-7233332" icon={<Phone />} />
-      <BottomNavigationAction label="Company@Banners.com" icon={<Email />} />
+    const navigate = useNavigate();
+    return (
+        <BottomNavigation
+            sx={{
+                width: "100%",
+                backgroundColor: "lightsteelblue",
+                zIndex: 2,
+            }}
+            showLabels
+        >
+            <BottomNavigationAction
+                onClick={() => navigate(ROUTES.home)}
+                label="management banner"
+                icon={<Extension />}
+                title="title for test"
+            />
+            <BottomNavigationAction label="050-7233332" icon={<Phone />} />
+            <BottomNavigationAction
+                label="Company@Banners.com"
+                icon={<Email />}
+            />
 
-      <BottomNavigationAction label="Twitter / X" icon={<Twitter />} />
-      <BottomNavigationAction label="Instagram" icon={<Instagram />} />
-      <BottomNavigationAction label="Facebook" icon={<Facebook />} />
-    </BottomNavigation>
-  );
+            <BottomNavigationAction label="Twitter / X" icon={<Twitter />} />
+            <BottomNavigationAction label="Instagram" icon={<Instagram />} />
+            <BottomNavigationAction label="Facebook" icon={<Facebook />} />
+            <BottomNavigationAction
+                href="https://github.com/program-training/Class5-Banners-front"
+                target="_blank"
+                label="GitHub"
+                icon={<GitHub />}
+            />
+        </BottomNavigation>
+    );
 };
 
 export default Footer;
